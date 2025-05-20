e370ba8561634dbd57bcf7c2aef7f31377a2a08f
xogur <snsk656@naver.com>
2025-05-20 21:50:11 +0900

    ch10.10

--- homework/ch10_8/wide_transform.py
+++ homework/ch10_8/wide_transform.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession
+'''from pyspark.sql import SparkSession
 from pyspark import SparkConf
 from pyspark.sql.functions import col, count
 from pyspark.sql.functions import broadcast
@@ -51,3 +51,52 @@ time.sleep(1200)
 
 # 세션 종료
 spark.stop()
+'''
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import broadcast, count
+import time
+
+spark = SparkSession \
+        .builder \
+        .appName('wide_transform.py') \
+        .config('spark.executor.memory', '2g') \
+        .config('spark.executor.instances', '3') \
+        .config('spark.executor.cores', '2') \
+        .config('spark.sql.adaptive.enabled', 'false') \
+        .getOrCreate()
+
+print(f'spark application start')
+job_skills_path = 'hdfs:///home/spark/sample/linkedin_jobs/jobs/job_skills.csv'
+job_skills_schema = 'job_id LONG, skill_abr STRING'
+skills_path = 'hdfs:///home/spark/sample/linkedin_jobs/mappings/skills.csv'
+skills_schema = 'skill_abr STRING, skill_name STRING'
+
+# job_skills load
+job_skills_df = spark.read \
+                 .option('header','true') \
+                 .schema(job_skills_schema) \
+                 .csv(job_skills_path)
+
+print(f'job_skills load 완료')
+
+# skills load
+skills_df = spark.read \
+                 .option('header','true') \
+                 .schema(skills_schema) \
+                 .csv(skills_path)
+
+print(f'skills load 완료')
+
+cnt_per_skills_df = job_skills_df.join(
+    other=broadcast(skills_df),
+    on='skill_abr',
+    how='inner'
+).select('job_id', 'skill_name') \
+    .groupBy('skill_name') \
+    .agg(count('job_id').alias('job_count')) \
+    .sort('job_count', ascending=False)
+
+print(cnt_per_skills_df.count())
+time.sleep(1200)
---
+++ lesson/ch10_10/read_csv_write_parquet.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession \
+    .builder \
+    .appName('read_csv_write_parquet') \
+    .getOrCreate()
+
+path = 'hdfs:///home/spark/sample/linkedin_jobs/companies/companies.csv'
+schema = 'company_id   STRING,' \
+         'name         STRING,' \
+         'description  STRING,' \
+         'company_size INT,' \
+         'state        STRING,' \
+         'country      STRING,' \
+         'city         STRING,' \
+         'zip_code     STRING,' \
+         'address      STRING,' \
+         'url          STRING'
+
+csv_df = spark \
+        .read \
+        .option("header", 'true') \
+        .option('multiLine','true') \
+        .schema(schema) \
+        .csv(path)
+csv_df.persist()
+print('Complete: Read companies.csv')
+csv_df.show()
+
+csv_df.write \
+        .format('parquet') \
+        .mode('overwrite') \
+        .save('hdfs:///home/spark/lesson/parquet/companies')
+
+# 아래와 같이 작성해도 동일하게 동작
+# csv_df.write.mode('overwrite').parquet('hdfs:///home/spark/lesson/parquet/companies')
+
+print('Complete: Save companies as parquet')
+
