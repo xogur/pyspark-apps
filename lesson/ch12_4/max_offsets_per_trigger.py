b71ea838249b3b6ed9c79831815ae7893be555d7
xogur <snsk656@naver.com>
2025-05-22 21:49:02 +0900

    ch12.4

---
+++ lesson/ch12_4/max_offsets_per_trigger.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+
+app_name = 'max_offsets_per_trigger'
+spark = SparkSession \
+        .builder \
+        .appName(app_name) \
+        .getOrCreate()
+
+kafka_source_df = spark.readStream \
+                .format("kafka") \
+                .option("kafka.bootstrap.servers", "kafka01:9092,kafka02:9092,kafka03:9092") \
+                .option("subscribe", "lesson.spark-streaming.test") \
+                .option('maxOffsetsPerTrigger','1000') \
+                .load()
+
+kafka_source_df = kafka_source_df.selectExpr(
+                    "CAST(key AS STRING) AS KEY",
+                    "CAST(value AS STRING) AS VALUE"
+                )
+
+query = kafka_source_df.writeStream \
+        .format('console') \
+        .option("checkpointLocation", f'/home/spark/kafka_offsets/{app_name}') \
+        .option("truncate", "false") \
+        .start()
+
+query.awaitTermination()
